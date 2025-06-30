791173447a948ca8d5eed9237f17445a31f2d872
Mmy2000 <mm.yousef811@gmail.com>
2025-06-30 10:06:40 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
