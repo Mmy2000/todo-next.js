48af97ef4c3926e9222c131fd750e9394e7bbde8
Mmy2000 <mm.yousef811@gmail.com>
2025-07-01 05:56:49 +0300

    feat: init chade cn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/app/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,15 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div className="flex-1 flex flex-col h-full">
-            {children}
-          </div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+            <div className="flex-1 flex flex-col h-full">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import MaxWidthWrapper from "@/components/ui/MaxWidthWrapper";
+import { ModeToggle } from "@/components/ui/ModeToggle";
+import { PlusIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-slate-50 grainy-light">
+      <div className="">
         <MaxWidthWrapper className="pb-24 pt-10 ">
-          <h1>Hello</h1>
-          <Button variant="default" className="cursor-pointer">
-            Click Me
-          </Button>
+          <h1 className="text-2xl font-bold text-slate-900 dark:text-slate-50">
+            Hello
+          </h1>
+          <div className="flex items-center gap-2">
+            <Button variant="default" className="cursor-pointer">
+              <PlusIcon size={14} className="mr-1" />
+              New Todo
+            </Button>
+          </div>
+          <ModeToggle />
         </MaxWidthWrapper>
       </div>
     </>
